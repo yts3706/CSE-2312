4bdffd72a3d13076038d31186c9810e866e05553
yts3706 <yash.singh@mavs.uta.edu>
2017-12-06 23:52:19 -0600

    Add files via upload

---
+++ yts3706_pFinal.s
@@ -0,0 +1,144 @@
+/****************
+* Yash Singh Final Program
+****************/
+
+.global main
+.func main
+
+main:
+	MOV R0, #0		@ index variable initialized 
+	BL _generate
+	PUSH {R10}
+	MOV R0, #0
+	BL _readloop
+	MOV R0, #0
+	BL _prompt
+	BL _sort_ascending
+	BL _getValue
+	MOV R8, R0
+	BL _search_value
+@	B _exit
+	
+_scanf:
+	PUSH {LR}
+    	SUB SP, SP, #4          @ make room on stack
+    	LDR R0, =format_str     @ R0 = address of string format
+    	MOV R1, SP              @ SP moved to R1 to store stack entry
+    	BL scanf                @ scanf called 
+    	LDR R0, [SP]            @ value loaded at SP into R0
+    	ADD SP, SP, #4          @ stack pointer restore
+    	POP {PC}	        @ return
+	
+_generate:
+	CMP R0, #0
+	PUSHEQ {LR}
+	MOVEQ R10, #0
+	CMP R0, #10		@ check and see iterating is done
+	POPEQ {PC}		@ loop if done = exit
+	LDR R1, =array_a	@ get address of a
+	LSL R2, R0, #2		@ index * 4 to get array offset
+	ADD R2, R1, R2		@ R2 = address 'a[R0]'
+	PUSH {R0}
+	PUSH {R1}
+	PUSH {R2}
+	BL _scanf
+	MOV R4, R0		@ value returned move of _scanf to R0
+	POP {R2}
+	POP {R1}
+	POP {R0}
+	STR R4, [R2]		@ user input = a[R0]
+	ADD R0, R0, #1		@ R0 + 1
+	B _generate	
+	
+_readloop:
+	CMP R0, #0
+	PUSHEQ {LR}
+	CMP R0, #10		@ see if iteration is done
+	P0OPEQ {PC}		@ exit loop if done
+	LDR R1, =array_a	@ address of a
+	LSL R2, R0, #2		@ index * 4 to get array offset
+	ADD R2, R1, R2		@ R2 = address a[R0]
+	LDR R1, [R2]		@ array 'a' at address
+	PUSH {R0} 		@ backup register before printf
+	PUSH {R1}		@ backup register before printf
+	PUSH {R2} 		@ backup register before printf
+	MOV R2, R1		@ array value moved to R2 for printf
+	MOV R1, R0		@ array index moved to R1 for printf
+	BL _printfA		@ branch to _printf procedure along with return
+	POP {R2}
+	POP {R1}
+	POP {R0}
+	ADD R0, R0, #1		@ index + 1
+	B _readloop		@ branch to next loop with iteration
+
+_printfA:
+	PUSH {LR}		@ return address stored
+	LDR R0, =printf_str	@ R0 contained within formatted string address
+	BL printf		@ printf called
+	POP {PC}		@ restore stack pointer then return
+	
+_sort_ascending:
+	PUSH {LR}
+
+_prompt:
+    MOV R7, #4              @ syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #22             @ string length printed
+    LDR R1, =prompt_str     @ string at label prompt_str:
+    SWI 0                   @ syscall executed
+    MOV PC, LR              @ return
+
+_getValue:
+	PUSH {LR}
+    	SUB SP, SP, #4          @ room on stack
+    	LDR R0, =format_str     @ R0 contained within address of format string
+    	MOV R1, SP              @ SP moved to R1 to store entry on stack
+    	BL scanf                @ scanf called
+    	LDR R0, [SP]            @ value loaded at SP into R0
+    	ADD SP, SP, #4          @ stack pointer restoed
+    	POP {PC}	        @ return
+
+
+_search_value:
+	CMP R5, #10
+	BEQ _search_complete
+	LDR R1, =array_a
+	LSL R2, R5, #2
+	ADD R2, R1, R2
+	LDR R3, [R2]
+	CMP R3, R8
+	LDREQ R0, =output_result
+	MOVEQ R1, R5
+	MOVEQ R2, R3
+	BLEQ printf
+	MOVEQ R10, #1
+	ADD R5, R5, #1
+	B _search_value		@ branch to next loop iteration
+
+_search_complete:
+    CMP R10, #0
+    LDREQ R0, =output_none
+    BLEQ printf
+
+
+@_exit:
+@	PUSH {LR}
+@	MOV R7, #4		@ write syscall, 4
+@	MOV R0, #1		@ output stream to monitor, 1
+@	MOV R2, #21		@ print string length
+@	LDR R1, =exit_str 	@ string at label exit_str:
+@	SWI 0			@ execute syscall
+@	MOV R7, #1		@ terminate syscall, 1
+@	SWI 0			@ execute syscall
+@	POP {PC}
+
+.data
+
+.balign	4
+array_a:	.skip		40
+printf_str: 	.asciz		"array_a[%d] = %d\n"
+prompt_str:	.asciz		"Enter a search value: "
+output_result:	.asciz		"array_a[%d] = %d\n"
+output_none:	.ascii		"That value does not exist in the array!\n"
+@exit_str:	.ascii		"Terminating Program.\n"
+format_str:	.asciz		"%d"
